0c0469b3f5f2d67b37468fe3bba9f06448e3d762
Jahongir Hamidov <hamidovjahongir444@gmail.com>
2025-01-07 18:00:09 +0500

    prod db changed

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-PRODUCTION_DB = 'postgresql://postgres:test123@localhost/TodoApplicationDatabase'
+PRODUCTION_DB = 'postgresql://database_rhhk_user:s8u3osWf7fUYpSDywRZ6Zc28IamuQ5iz@dpg-ctui9gi3esus739cml1g-a.oregon-postgres.render.com/database_rhhk'
 TEST_DB = 'sqlite:///./todosapp.db'
 
-engine = create_engine(TEST_DB, connect_args={'check_same_thread': False})
+engine = create_engine(PRODUCTION_DB, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
